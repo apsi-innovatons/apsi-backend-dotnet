9d7f8d35245eeb1421d7423ec73f5a80e55e603e
Gabriel Rębacz <gabriel.rebacz@enigma.com.pl>
2021-11-01 16:08:42 +0100

    swagger in release

--- Startup.cs
+++ Startup.cs
@@ -40,9 +40,10 @@ namespace apsi.backend.social
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "apsi.backend.social v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "apsi.backend.social v1"));
 
             app.UseHttpsRedirection();
 
