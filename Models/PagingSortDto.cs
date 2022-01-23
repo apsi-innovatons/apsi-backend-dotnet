889a3906bc809de029da586ff8ef1a5b4a072f30
krzepka <rzarol.kepka@gmail.com>
2022-01-23 13:18:07 +0100

    add PagingSortDto

---
+++ Models/PagingSortDto.cs
@@ -0,0 +1,15 @@
+﻿using apsi.backend.social.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apsi.Backend.Social.Models
+{
+    public class PagingSortDto: PagingDto
+    {
+        [DefaultValue(false)]
+        public bool SortDate { get; set; }
+    }
+}
